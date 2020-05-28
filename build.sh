8fcef65047f736b97c5b04258bd2f300697f8054
herrywen-nanj <33476935+herrywen-nanj@users.noreply.github.com>
2020-05-28 12:29:09 +0800

    打包并上传至腾讯云

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
 echo "开启打包过程：docker build -t sqhx ."
+rsync -av --exclude=tools ./*   ./tools
+# 服务器关机的话，需要进入容器执行docker login登录腾讯云
+cd tools && docker build -t sqhx_java .  && docker tag sqhx_java    && docker push 
+
