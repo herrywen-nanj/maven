b1d1abd38f1b409e7687a80c7ebd0dd8e4138718
zxadmin <hq-xwkjxmgl@chinaunicom.cn>
2020-05-29 13:17:44 +0800

    add content

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 # !/bin/bash -l
 echo "开启打包过程：docker build -t sqhx ."
-rsync -av --exclude=tools ./*   ./tools
+rsync --version
+#rsync -av --exclude=tools ./*   ./tools
 docker images
 # 服务器关机的话，需要进入容器执行docker login登录腾讯云
 #cd tools && docker build -t sqhx_java .  && docker tag sqhx_java  ccr.ccs.tencentyun.com/miwan/sqhx_java:latest  && docker push ccr.ccs.tencentyun.com/miwan/sqhx_java:latest
